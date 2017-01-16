63a24f9825371873224fc8905ae73d42c49ab5da
Stéphane P. Péricat <contact@pericat.net>
2017-01-16 15:34:30 -0600

    code coverage

--- backpack.config.js
+++ backpack.config.js
@@ -1,4 +1,3 @@
-var webpack = require('webpack');
 // backpack.config.js
 // IMPORTANT: This file is not going through babel transformation.
 // You can however use the ES2015 features supported by your Node.js version.
@@ -6,15 +5,8 @@ module.exports = {
   /* config options here */
   webpack: (config, options) => {
     // Perform customizations to config
+
     // Important: return the modified config
-
-    // uglify build
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }));
-
     return config;
   }
 }
