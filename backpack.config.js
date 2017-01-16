679b2e970797b697ef961ca11cf09f7b7c5ba863
Stéphane P. Péricat <contact@pericat.net>
2017-01-16 15:02:19 -0600

    setup backpack

---
+++ backpack.config.js
@@ -0,0 +1,6 @@
+// backpack.config.js
+// IMPORTANT: This file is not going through babel transformation.
+// You can however use the ES2015 features supported by your Node.js version.
+module.exports = {
+  /* config options here */
+}
